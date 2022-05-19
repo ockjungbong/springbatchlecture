ccee75bb5eedac7c7c4840272f95c4eb77d76331
devock <ockjungbong@gmail.com>
2022-05-19 23:31:31 +0900

    테스트

--- src/main/java/io/springbatch/springbatchlecture/HelloJobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/HelloJobConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class HelloJobConfiguration {
 	
 	private final JobBuilderFactory jobBuilderFactory;
